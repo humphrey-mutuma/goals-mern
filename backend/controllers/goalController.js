177e6fe41cd0ced34994650d8b555edf86313ccd
humphrey-mutuma <humphreymutuma01@gmail.com>
2022-06-11 12:56:18 +0300

    set routes to async

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,32 +1,34 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc get goals
 // @route GET /api/goals
 // @access private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get Goals" });
-};
+});
 
 // @desc set goals
 // @route POST /api/goals
 // @access private
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text input");
   }
   res.status(200).json({ message: "Set Goals" });
-};
+});
 
 // @desc update a goal
 // @route PUT /api/goals/:id
 // @access private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `update Goal ${req.params.id}` });
-};
+});
 // @desc delete goal
 // @route DELETE /api/goals/:id
 // @access private
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `delete Goal ${req.params.id}` });
-};
+});
 
 module.exports = { getGoals, setGoals, updateGoal, deleteGoals };
