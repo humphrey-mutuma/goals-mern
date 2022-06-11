e616d34ef019aca23c58d0a754d6a25bde5e4d02
humphrey-mutuma <humphreymutuma01@gmail.com>
2022-06-11 10:47:39 +0300

    Get goals route

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,6 @@ const dotenv = require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  console.log("Hello there!!");
-  res.send("Hello there");
-});
+app.use("/api/goals", require("../routes/goalRoutes"));
 
 app.listen(PORT, console.log(`Server running on port:${PORT}`));
