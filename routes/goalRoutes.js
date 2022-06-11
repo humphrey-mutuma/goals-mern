5a1c924c399d4167e009f4932317fe1f8254d558
humphrey-mutuma <humphreymutuma01@gmail.com>
2022-06-11 11:09:47 +0300

    goals crud apis setup.

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
+// get a goal route
 router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get some goals" });
+  res.status(200).json({ message: "Get goals" });
+});
+
+// post a goal route
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "Set goal" });
+});
+// update a goal
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `update goal ${req.params.id}` });
+});
+// delete a goal
+
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: `delete goal ${req.params.id}` });
 });
 
 module.exports = router;
