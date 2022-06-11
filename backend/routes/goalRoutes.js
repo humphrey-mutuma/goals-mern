aee1b859571bd4eadf1a66fec72f32c270878698
humphrey-mutuma <humphreymutuma01@gmail.com>
2022-06-11 11:42:12 +0300

    update common routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,16 +7,18 @@ const {
   deleteGoals,
 } = require("../controllers/goalController");
 
-
 // get a goal route
 router.get("/", getGoals);
-
 // post a goal route
 router.post("/", setGoals);
+
+// router.route('/').get(getGoals).post(setGoals) 
+
 // update a goal
 router.put("/:id", updateGoal);
 // delete a goal
-
 router.delete("/:id", deleteGoals);
+ 
+// router.route('/:id').put(updateGoal).delete(deleteGoals) 
 
 module.exports = router;
