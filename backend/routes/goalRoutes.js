ea78dc6a7a3027b7c7b5d4c2d11611476ba736cd
humphrey-mutuma <humphreymutuma01@gmail.com>
2022-06-12 14:05:48 +0300

    protect all goal routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,19 +6,20 @@ const {
   updateGoal,
   deleteGoals,
 } = require("../controllers/goalController");
+const { protect } = require("../middleware/authMiddleware");
 
 // get a goal route
 // router.get("/", getGoals);
 // // post a goal route
 // router.post("/", setGoals);
 
-router.route('/').get(getGoals).post(setGoals) 
+router.route("/").get(protect, getGoals).post(protect, setGoals);
 
 // // update a goal
 // router.put("/:id", updateGoal);
 // // delete a goal
 // router.delete("/:id", deleteGoals);
- 
-router.route('/:id').delete(deleteGoals).put(updateGoal)
+
+router.route("/:id").delete(protect, deleteGoals).put(protect, updateGoal);
 
 module.exports = router;
