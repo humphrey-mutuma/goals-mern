2d8451714f5517338585eddc290010328485d98c
humphrey-mutuma <humphreymutuma01@gmail.com>
2022-06-12 14:27:04 +0300

    Auth && authentication complete

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,14 @@
 const asyncHandler = require("express-async-handler");
 const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
 
 // @desc get goals
 // @route GET /api/goals
 // @access private
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  // const goals = await Goal.find(); GET all goals
+  const goals = await Goal.find({ user: req.user.id }); // get goals based on logged in user
+
   res.status(200).json(goals);
 });
 
@@ -19,6 +22,7 @@ const setGoals = asyncHandler(async (req, res) => {
   }
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   });
   res.status(200).json(goal);
 });
@@ -31,6 +35,17 @@ const updateGoal = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400);
     throw new Error("Goal not found");
+  }
+  const user = await User.findById(req.user.id);
+  // check for a user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+  // make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -45,6 +60,17 @@ const deleteGoals = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400);
     throw new Error("Goal not found");
+  }
+  const user = await User.findById(req.user.id);
+  // check for a user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+  // make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
   await goal.remove();
   res.status(200).json({ id: req.params.id });
